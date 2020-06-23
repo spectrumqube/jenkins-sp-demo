75062c07b8efad8f099d5e7a04781a9b13c59bf8
spectrumqube <34798890+spectrumqube@users.noreply.github.com>
2020-06-24 00:28:26 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application'
+/* echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
@@ -23,4 +23,7 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar target/${NAME}-${VERSION}.jar 
+*/
+
+echo "Deliver Stage"
